90f388a508167ece90e648bfb8c83fe8735e40f3
Siddhesh1123 <101471387+Siddhesh1123@users.noreply.github.com>
2024-11-27 23:33:16 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -54,7 +54,7 @@ const Dashboard = () => {
     setFormData({
       username: user.username,
       email: user.email,
-      password: "", // Optional: Do not pre-fill password during editing
+      password: user.password, // Optional: Do not pre-fill password during editing
       role: user.role._id,
     });
     setCurrentUserId(user._id); // Store the current user's ID to send in the update request
@@ -64,12 +64,13 @@ const Dashboard = () => {
     if (window.confirm("Are you sure you want to delete this user?")) {
       try {
         const response = await fetch(
-          `http://localhost:5000/api/users/${userId}`,
+         ` http://localhost:5000/api/users/${userId}`,
           {
             method: "DELETE",
-            headers: `{ Authorization: Bearer ${auth.token} }`,
+            headers: { Authorization: `Bearer ${auth.token}` },
           }
         );
+        console.log("response", response);
         if (response.ok) {
           alert("User deleted successfully");
           fetchUsers(); // Refresh users
@@ -117,14 +118,14 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-base-200 ">
-      <div className="bg-gray-800 p-6 rounded-lg shadow-lg w-full m-20">
+    <div className="flex justify-center items-center min-h-screen bg-white ">
+      <div className="bg-slate-200 p-6 rounded-lg shadow-lg w-full m-20">
         <div className="flex flex-row justify-between items-center">
           <div>
-            <h1 className="text-6xl font-bold text-left text-slate-400  mb-4">
+            <h1 className="text-6xl font-bold text-left text-slate-800  mb-4">
               Welcome, {auth.user.username}!
             </h1>
-            <p className="text-4xl text-left text-slate-500  mb-6">
+            <p className="text-4xl text-left text-slate-700  mb-6">
               Your Role is :{" "}
               <span className="font-semibold text-blue-500">
                 {getUserRole(auth.user.role)}
@@ -143,7 +144,7 @@ const Dashboard = () => {
 
         <div className="mt-14">
           <div className="flex flex-row items-center justify-between">
-          <h2 className="text-3xl font-semibold text-slate-400  mb-3 flex justify-between">
+          <h2 className="text-3xl font-semibold text-slate-600  mb-3 flex justify-between">
             User List
             
           </h2>
@@ -156,16 +157,16 @@ const Dashboard = () => {
               </button>
             )}
             </div>
-          <table className="w-full table-auto border-collapse mt-10">
-            <thead>
-              <tr>
+          <table className="w-full table-auto border-collapse border-black mt-10 bg-slate-100">
+            <thead >
+              <tr className="text-gray-600 ">
                 <th className="border px-4 py-2 text-2xl">Username</th>
                 <th className="border px-4 py-2 text-2xl">Email</th>
                 <th className="border px-4 py-2 text-2xl">Role</th>
                 <th className="border px-4 py-2 text-2xl">Actions</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="text-gray-600">
               {users.map((user) => (
                 <tr key={user._id}>
                   <td className="border px-4 py-2 text-xl ">{user.username}</td>
@@ -198,54 +199,52 @@ const Dashboard = () => {
 
         {/* Modal */}
         {isModalOpen && (
-          <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
-            <div className="bg-gray-800 p-6 rounded shadow-lg w-96">
-              <h2 className="text-xl font-bold mb-4">
+          <div className="fixed inset-0 bg-gray-200 bg-opacity-50 flex justify-center items-center">
+            <div className="bg-gray-200 p-6 rounded shadow-lg w-96">
+              <h2 className="text-xl text-slate-700 font-bold mb-4">
                 {isEditing ? "Edit User" : "Add User"}
               </h2>
               <form onSubmit={handleSubmit}>
                 <div className="mb-4">
-                  <label className="block text-white">Username</label>
+                  <label className="block text-black">Username</label>
                   <input
                     type="text"
                     name="username"
                     value={formData.username}
                     onChange={handleFormChange}
-                    className="w-full px-3 py-2 border rounded"
+                    className="w-full bg-slate-800 px-3 py-2 border rounded"
                     required
                   />
                 </div>
                 <div className="mb-4">
-                  <label className="block text-white">Email</label>
+                  <label className="block text-black">Email</label>
                   <input
                     type="email"
                     name="email"
                     value={formData.email}
                     onChange={handleFormChange}
-                    className="w-full px-3 py-2 border rounded"
+                    className="w-full bg-slate-800 px-3 py-2 border rounded"
                     required
                   />
                 </div>
-                {!isEditing && (
                   <div className="mb-4">
-                    <label className="block text-white">Password</label>
+                    <label className="block text-black">Password</label>
                     <input
                       type="password"
                       name="password"
                       value={formData.password}
                       onChange={handleFormChange}
-                      className="w-full px-3 py-2 border rounded"
+                      className="w-full bg-slate-800 px-3 py-2 border rounded"
                       required
                     />
                   </div>
-                )}
                 <div className="mb-4">
-                  <label className="block text-white">Role</label>
+                  <label className="block text-black">Role</label>
                   <select
                     name="role"
                     value={formData.role}
                     onChange={handleFormChange}
-                    className="w-full px-3 py-2 border rounded"
+                    className="w-full bg-slate-800 px-3 py-2 border rounded"
                     required
                   >
                     <option value="">Select Role</option>
@@ -257,7 +256,7 @@ const Dashboard = () => {
                 <div className="flex justify-between">
                   <button
                     type="button"
-                    className="bg-gray-400 text-white px-4 py-2 rounded hover:bg-gray-500"
+                    className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-500"
                     onClick={() => setIsModalOpen(false)}
                   >
                     Cancel
