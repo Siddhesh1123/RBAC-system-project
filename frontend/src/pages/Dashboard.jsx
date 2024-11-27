dd63ceab777c7cf9f87585860b75421ea1b4ec13
Siddhesh1123 <101471387+Siddhesh1123@users.noreply.github.com>
2024-11-28 00:27:29 +0530

    Update Dashboard.jsx styles

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,9 @@
 import React, { useContext, useState, useEffect } from "react";
 import AuthContext from "../context/AuthContext";
 import { getUserRole } from "../context/GetUserRoles";
+import Dashbaordanimation from "../assests/Dashani.json";
+import Rocket from "../assests/Rocketanimation.json";
+import Lottie from "lottie-react";
 
 const Dashboard = () => {
   const { auth, logout } = useContext(AuthContext);
@@ -14,6 +17,8 @@ const Dashboard = () => {
   });
   const [isEditing, setIsEditing] = useState(false); // Track whether it's an edit or add operation
   const [currentUserId, setCurrentUserId] = useState(null); // Track the current user to edit
+  const [isLoading, setIsLoading] = useState(true);  // Added loading state
+  const [isDataLoaded, setIsDataLoaded] = useState(false);  // Track if data is loaded
 
   useEffect(() => {
     fetchUsers();
@@ -24,19 +29,22 @@ const Dashboard = () => {
       const response = await fetch("http://localhost:5000/api/users", {
         method: "GET",
         headers: {
-          Authorization:` Bearer ${auth.token}`,
+          Authorization: ` Bearer ${auth.token}`,
           "Content-Type": "application/json",
         },
       });
-      console.log("response", response);
       if (response.ok) {
         const data = await response.json();
         setUsers(data);
+        setIsLoading(false); // Stop loading once data is fetched
+        setIsDataLoaded(true);  // Set data as loaded
       } else {
         console.error("Failed to fetch users");
+        setIsLoading(false); // Stop loading on error
       }
     } catch (error) {
       console.error("Error fetching users:", error);
+      setIsLoading(false); // Stop loading on error
     }
   };
 
@@ -64,13 +72,12 @@ const Dashboard = () => {
     if (window.confirm("Are you sure you want to delete this user?")) {
       try {
         const response = await fetch(
-         ` http://localhost:5000/api/users/${userId}`,
+          ` http://localhost:5000/api/users/${userId}`,
           {
             method: "DELETE",
             headers: { Authorization: `Bearer ${auth.token}` },
           }
         );
-        console.log("response", response);
         if (response.ok) {
           alert("User deleted successfully");
           fetchUsers(); // Refresh users
@@ -99,7 +106,6 @@ const Dashboard = () => {
         },
         body: JSON.stringify(formData),
       });
-      console.log("response", response);
 
       if (response.ok) {
         alert(`User ${isEditing ? "updated" : "added"} successfully`);
@@ -108,7 +114,7 @@ const Dashboard = () => {
       } else {
         alert(`Failed to ${isEditing ? "update" : "save"} user`);
       }
-    }catch (error) {
+    } catch (error) {
       console.error("Error saving user:", error);
     }
   };
@@ -118,86 +124,107 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-white ">
-      <div className="bg-slate-200 p-6 rounded-lg shadow-lg w-full m-20">
-        <div className="flex flex-row justify-between items-center">
-          <div>
-            <h1 className="text-6xl font-bold text-left text-slate-800  mb-4">
-              Welcome, {auth.user.username}!
-            </h1>
-            <p className="text-4xl text-left text-slate-700  mb-6">
-              Your Role is :{" "}
-              <span className="font-semibold text-blue-500">
-                {getUserRole(auth.user.role)}
-              </span>
-            </p>
-          </div>
-          <div className="flex justify-center">
-            <button
-              className="bg-blue-900 text-white px-10 py-2 text-xl rounded hover:bg-blue-800 h-14"
-              onClick={handleLogout}
-            >
-              Logout
-            </button>
-          </div>
+    <div className="container mx-auto px-4 min-h-screen flex flex-col bg-white p-4 relative">
+      {isLoading && (
+        <div className="w-full h-screen absolute top-0 left-0 flex justify-center items-center bg-gray-200 bg-opacity-50 z-10">
+          <Lottie animationData={Dashbaordanimation} loop={true} />
         </div>
-
-        <div className="mt-14">
-          <div className="flex flex-row items-center justify-between">
-          <h2 className="text-3xl font-semibold text-slate-600  mb-3 flex justify-between">
-            User List
-            
-          </h2>
-          {getUserRole(auth.user.role) === "Admin" && (
-              <button
-                className="bg-green-500 text-white px-4 py-2 text-xl rounded hover:bg-green-600"
-                onClick={handleAddUser}
-              >
-                Add User
-              </button>
-            )}
+      )}
+      
+      {!isLoading && isDataLoaded && (
+        <div>
+          <div className="mt-0 md:mt-14 flex flex-col md:flex-row justify-between items-start md:items-center space-y-4 md:space-y-0 mb-6 border-b-4 border-slate-200 pb-6">
+            <div>
+              <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold text-gray-700 mb-2">
+                Welcome, {auth.user.username}!
+              </h1>
+              <p className="text-xl md:text-2xl lg:text-4xl text-gray-500">
+                Your Role is:{" "}
+                <span className="font-semibold text-blue-500">
+                  {getUserRole(auth.user.role)}
+                </span>
+              </p>
             </div>
-          <table className="w-full table-auto border-collapse border-black mt-10 bg-slate-100">
-            <thead >
-              <tr className="text-gray-600 ">
-                <th className="border px-4 py-2 text-2xl">Username</th>
-                <th className="border px-4 py-2 text-2xl">Email</th>
-                <th className="border px-4 py-2 text-2xl">Role</th>
-                <th className="border px-4 py-2 text-2xl">Actions</th>
-              </tr>
-            </thead>
-            <tbody className="text-gray-600">
-              {users.map((user) => (
-                <tr key={user._id}>
-                  <td className="border px-4 py-2 text-xl ">{user.username}</td>
-                  <td className="border px-4 py-2 text-xl">{user.email}</td>
-                  <td className="border px-4 py-2 text-xl">{user.role.name}</td>
-                  <td className="border px-4 py-2 flex space-x-2">
-                    {(getUserRole(auth.user.role) === "Admin" ||
-                      getUserRole(auth.user.role) === "Editor") && (
-                      <>
-                        <button
-                          className="bg-blue-500 text-white px-10 py-2 rounded hover:bg-blue-600"
-                          onClick={() => handleEditUser(user)}
-                        >
-                          Edit
-                        </button>
-                        <button
-                          className="bg-red-500 text-white px-10 py-2  rounded hover:bg-red-600"
-                          onClick={() => handleDeleteUser(user._id)}
-                        >
-                          Delete
-                        </button>
-                      </>
-                    )}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            <div>
+              <button
+                className="bg-orange-400 text-white text-xl px-10 py-2 rounded hover:bg-orange-500"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </div>
+          </div>
 
-        {/* Modal */}
+          {/* Hero Section */}
+          <div className="mt-0 md:mt-14 hero flex flex-col lg:flex-row items-center justify-between space-y-6 lg:space-y-0 lg:space-x-8 mb-12">
+            <div className="w-full lg:w-1/2">
+              <Lottie animationData={Dashbaordanimation} />
+            </div>
+            <div className="w-full lg:w-1/2">
+              <h1 className="text-4xl lg:text-6xl font-bold text-gray-700 mb-4">
+                Role-Based Access Control
+              </h1>
+              <p className="text-xl lg:text-2xl text-gray-500">
+                Manage users, permission, and role seamlessly, ensuring secure
+                access for every role in your system.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-full bg-white p-4 md:p-10 rounded-lg">
+            <div className="flex flex-col md:flex-row items-left md:items-center justify-between mb-6">
+              <h2 className="text-3xl md:text-4xl font-semibold text-gray-800 mb-4 md:mb-0">
+                User List
+              </h2>
+              {getUserRole(auth.user.role) === "Admin" && (
+                <button
+                  className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 text-xl"
+                  onClick={handleAddUser}
+                >
+                  Add User
+                </button>
+              )}
+            </div>
+            <div className="overflow-x-auto">
+              <table className="w-full table-auto">
+                <thead>
+                  <tr className="bg-gray-200 ">
+                    <th className="px-2 md:px-4 py-2 text-sm md:text-xl text-gray-800">Username</th>
+                    <th className="px-2 md:px-4 py-2 text-sm md:text-xl text-gray-800">Email</th>
+                    <th className="px-2 md:px-4 py-2 text-sm md:text-xl text-gray-800">Role</th>
+                    <th className="px-2 md:px-4 py-2 text-sm md:text-xl text-gray-800">Actions</th>
+                  </tr>
+                </thead>
+                <tbody className="text-gray-600">
+                  {users.map((user) => (
+                    <tr key={user._id}>
+                      <td className="px-2 md:px-4 py-2 text-sm md:text-xl text-gray-800 text-center ">{user.username}</td>
+                      <td className="px-2 md:px-4 py-2 text-sm md:text-xl text-gray-800 text-center">{user.email}</td>
+                      <td className="px-2 md:px-4 py-2 text-sm md:text-xl text-gray-800 text-center">{user.role.name}</td>
+                      <td className="px-2 md:px-4 py-2 text-sm md:text-xl text-gray-800 text-center space-x-2">
+                        {getUserRole(auth.user.role) === "Admin" && (
+                          <>
+                            <button
+                              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 text-xl"
+                              onClick={() => handleEditUser(user)}
+                            >
+                              Edit
+                            </button>
+                            <button
+                              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 text-xl"
+                              onClick={() => handleDeleteUser(user._id)}
+                            >
+                              Delete
+                            </button>
+                          </>
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            {/* Modal */}
         {isModalOpen && (
           <div className="fixed inset-0 bg-gray-200 bg-opacity-50 flex justify-center items-center">
             <div className="bg-gray-200 p-6 rounded shadow-lg w-96">
@@ -272,7 +299,10 @@ const Dashboard = () => {
             </div>
           </div>
         )}
-      </div>
+
+          </div>
+        </div>
+      )}
     </div>
   );
 };
